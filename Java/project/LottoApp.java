52dd75e5dc028f3ceb39a502d4ef96a7de913adb
witoldzol <blue3656@gmail.com>
2017-11-15 08:22:07 +0000

    arrays

--- Java/project/LottoApp.java
+++ Java/project/LottoApp.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class LottoApp{
 
@@ -7,52 +9,91 @@ public class LottoApp{
 
 	//vars
 	int[][] pickedNumbers = new int[3][6];
-	String userInput, filteredUserInput;
+	String[] input, filteredUserInput;
 	int temp;
 	
 	//prompt for numbers
-	for(int i=0; i<3;i++){
-	    queryForNumbers();
+	for(int i=0; i<1;i++){
+	    //ask user for numbers
+	    input = queryForNumbers(i);
+	    //use input to insert it into our holding array
+	    //	    insertInArray(i,pickedNumbers,userInput);
+	    //	    System.out.println("line " + i + "is" + Arrays.deepToString(pickedNumbers));
+	    System.out.println(Arrays.toString(input));
 	}
 	
 	
     }
-    //method for removing all non digit characters
-    //in case user inputs incorrect characters
-    //or tries to mess with our program
-    public static String removeNonDigits(String x){
-	//vars
-	int len = x.length();
-	String str;
-	//obj-buffer
-	StringBuffer sb = new StringBuffer();
-	//loop through each char and test if it's a digit
-	for(int i=0; i<len; i++){
-	    if( Character.isDigit( x.charAt(i) ) ){
-		//insert digit into buffer
-		sb.append(x.charAt(i));
-	    }
-	    
+    /*
+    //method for inserting digits into array
+    public static int[][] insertInArray(int x, int[][] y, String userInput){
+	//var
+
+	//loop
+	for(int i=0;i<userInput.length();i++){
+	    // because charAt() gives as unicode values
+	    //by substracting result with '0' we convert them to
+	    //standard didgits
+	    y[x][i] = userInput.charAt(i) - '0';
 	}
-	//transform buffer to string after we are done with the loop
-	str = sb.toString();
-	return str;
+	
+	return y;
+    }
+    */
+    
+    //method for removing all non digit characters
+    //it will also check if numbers are valid (0-40)
+    //
+    public static String[] removeNonDigits(String x){
+	//vars
+	int counter = 0;
+	String [] arr = new String[x.length()];
+	String [] filteredArr = new String[6];
+	//break string into array
+	//this will remove all non numeric numbers at the same time
+	arr = x.split("[^0-9]");
+	//loop through each char and test if it's a digit
+	for(int i=0; i<arr.length; i++){
+	    if (arr[i] != ""){
+		filteredArr[counter] = arr[i];
+		counter++;
+	    }
+	}
+	return filteredArr;
     }
 
-    public static String  queryForNumbers(){
-	
-	String userInput;
+    public static String[]  queryForNumbers(int x){
+	//vars
+	String[] userInput = new String[6];
+	ArrayList<Integer> al = new ArrayList<Integern>();
 	//scanner
 	Scanner scan = new Scanner(System.in);
 	
-	System.out.println("Please pick six numbers, from 1 to 10");
+	System.out.println("please pick six numbers, from 1 to 40");
 	    //filter user input and remove digits
 	userInput = removeNonDigits(scan.nextLine());
-	    //check if we have correct number of digits
-	System.out.println("you selected: " + userInput);
-	if(userInput.length() != 6){
-	    queryForNumbers();
+	//test if user didn't enter invalid numbers outside permitted range
+	test = testNumbers(userInput);
+	//call the method again, if we didn't get 6 digits
+	if(userInput.length != 6){
+	    System.out.println("You must enter 6 digits, try again!");
+	    queryForNumbers(x);
 	}
 	return userInput;
     }
+    //method for testing numbers if they are correct
+    public static ArrayList<Integer> testNumbers(String[] x){
+
+	ArrayList al = new ArrayList();
+	
+	for(int i=0;i<x.length;i++){
+	    //convert values to integers so we can test them
+	    int y = Integer.parseInt(x[i]);
+	    //if any value is out of range, add it to test al
+	    if( y >40 || y<0){
+		al.add(y);
+	    }
+	}
+    }
+    
 }
